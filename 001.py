0cbc6a48969f0d6603c35b45ed70f517663680dd
qiao <840569784@qq.com>
2017-03-12 11:42:55 +0800

    添加新文件001.py

---
+++ 001.py
@@ -0,0 +1,33 @@
+# -*-coding:utf-8*-
+
+
+class People(object):
+    def __init__(self, age):
+        print 'P'
+        self.age = age
+
+    def getinfo(self):
+        return self.age
+
+    def setinfo(self, newage):
+        self.age = newage
+    x = property(getinfo, setinfo)
+
+
+class Student(People):
+    def __init__(self, age):
+        print 'S'
+        self.age = age
+    # def info(self):
+    #     print self.name + ':' + str(self.score)
+    #     print self.age
+
+
+stu1 = Student(22)
+# stu1.name = '乔'
+# stu1.score = 100
+# stu1.info()
+# stu1.getinfo()
+print stu1.x
+stu1.x = 18
+print stu1.x
